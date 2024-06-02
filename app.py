2b1839fa15a4c1f6d91a8fd7209a88f464466048
Supriyo Maity <supriyo.codes@gmail.com>
2024-06-02 20:32:06 +0530

    second commit

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-from flask import Flask,render_template,request,jsonify
-import os
-import markdown2
-from langchain_google_genai import GoogleGenerativeAI
+
+# Import the Flask class from the flask module
+from flask import Flask, render_template
+
+# Create an instance of the Flask class
 app = Flask(__name__)
-os.environ['GOOGLE_API_KEY'] = 'AIzaSyAvGhNC698GNrRZbhXv9BxmDErHqSAxmlU'
-llms = GoogleGenerativeAI(model="gemini-pro")
 
- 
-
+# Define a route for the root URL ("/")
 @app.route('/')
-def index():
-    return render_template("index.html")
- 
+def chat():
+    return render_template ("index.html")
 
-@app.route('/', methods=['post'])
-def getdata():
-     if request.is_json:
-         data = request.json['sendQuery']
-         print(data)
-         
-         result=llms.predict(data)
-         text = markdown2.markdown(result)
-         print(text)
-         return jsonify({'message': text})
-
+# Check if the executed file is the main program and run the app
 if __name__ == '__main__':
     app.run(debug=True)
